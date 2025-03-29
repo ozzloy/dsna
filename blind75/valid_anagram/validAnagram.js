8c99861cb33de55b07f6515d4a5982e95b00e9d6
daniel watson <ozzloy@each.do>
2025-03-28 18:46:47 -0700

    start valid anagram in js

---
+++ blind75/valid_anagram/validAnagram.js
@@ -0,0 +1,30 @@
+class Solution {
+  /**
+   * @param {string} s
+   * @param {string} t
+   * @return {boolean}
+   */
+  isAnagram(s, t) {}
+}
+
+s00 = "racecar";
+t00 = "carrace";
+expect00 = true;
+
+s01 = "jar";
+t00 = "jam";
+expect00 = false;
+
+const test = (name, s, t, expect) => {
+  const solver = new Solution();
+  const actual = solver.isAnagram(s, t);
+  if (expect === actual) return;
+  console.log("name", name);
+  console.log("nums", s);
+  console.log("expect", expect);
+  console.log("actual", actual);
+  process.exit(1);
+};
+
+test("00", s00, t00, expect00);
+test("01", s01, t01, expect01);
